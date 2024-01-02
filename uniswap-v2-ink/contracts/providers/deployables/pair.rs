90c555fe29ea8d553bd4d1afaae7f3d6bbe55786
Nwele Uchenna (developeruche) <developeruche@gmail.com>
2024-01-02 12:35:56 +0100

    mod: done with the mint function

--- uniswap-v2-ink/contracts/providers/deployables/pair.rs
+++ uniswap-v2-ink/contracts/providers/deployables/pair.rs
@@ -1,4 +1,6 @@
 
+use core::time;
+
 use ink::prelude::vec::Vec;
 use ink::primitives::AccountId;
 use openbrush::{traits::{Storage, Balance}, contracts::{psp22::{psp22, extensions::metadata::PSP22MetadataImpl}, traits::psp22::PSP22Ref}};
@@ -53,22 +55,22 @@ pub trait PairImpl:
     }
 
     fn mint(&mut self, _to: AccountId) -> Result<(), UniswapV2Errors> {
-        let state = *self.data::<PairStorage>();
+        let mut state = *self.data::<PairStorage>();
         let caller = Self::env().caller();
-        let (reserve_0, reserve_1, block_timestamp_last) = self.get_reserves();
+        let (reserve_0, reserve_1, minimum_liquidity) = (state.reserve_0, state.reserve_1, state.minimum_liquidity);
         let balance0 = PSP22Ref::balance_of(&state.token_0, caller);
         let balance1 = PSP22Ref::balance_of(&state.token_1, caller);
         let amount0 = balance0 - reserve_0;
         let amount1 = balance1 - reserve_1;
         let liqudity: Balance;
 
-        let feeOn = self.internal_mint_fee();
+        let fee_on = self.internal_mint_fee();
 
 
         if self._total_supply() == 0 {
             // this is the first liquidity is been added to the pool
-            liqudity = internal_sqrt(amount0 * amount1) - state.minimum_liquidity;
-            self._mint_to(AccountId::from([0u8;32]), state.minimum_liquidity)?;
+            liqudity = internal_sqrt(amount0 * amount1) - minimum_liquidity;
+            self._mint_to(AccountId::from([0u8; 32]), minimum_liquidity)?;
         } else {
             liqudity = (amount0 * self._total_supply() / reserve_0).min(amount1 * self._total_supply() / reserve_1);
         }
@@ -79,29 +81,16 @@ pub trait PairImpl:
 
         self._mint_to(_to, liqudity)?;
 
-        
-        // let liquidity = if reserve_0 == 0 && reserve_1 == 0 {
-        //     amount0 * amount1
-        // } else {
-        //     amount0.min(amount1) * 1000 / amount0.max(amount1)
-        // };
-        // if liquidity == 0 {
-        //     return Err(UniswapV2Errors::PSP22Error(psp22::PSP22Error::InsufficientBalance));
-        // }
-        // let mint_liquidity = liquidity * 1000 / 997;
-        // let mint_amount0 = amount0 * mint_liquidity / liquidity;
-        // let mint_amount1 = amount1 * mint_liquidity / liquidity;
-        // if mint_amount0 == 0 || mint_amount1 == 0 {
-        //     return Err(UniswapV2Errors::PSP22Error(psp22::PSP22Error::InsufficientBalance));
-        // }
-        // self.internal_transfer(state.token_0, _to, mint_amount0);
-        // self.internal_transfer(state.token_1, _to, mint_amount1);
-        // state.reserve_0 += mint_amount0;
-        // state.reserve_1 += mint_amount1;
-        // state.block_timestamp_last = block_timestamp_last;
+        self.internal_update(balance0, balance1)?;
+
+        if fee_on.unwrap_or(false) {
+            self.data::<PairStorage>().k_last = (reserve_0 as u128) * (reserve_1 as u128);
+        }
+
         Ok(())
     }
 
+    
 
 
 
@@ -115,7 +104,7 @@ pub trait PairImpl:
         return PSP22Ref::transfer(&_token, _to, _value, Vec::<u8>::new()).is_ok();
     }
 
-    fn internal_mint_fee(&mut self) -> bool {
+    fn internal_mint_fee(&mut self) -> Result<bool, UniswapV2Errors> {
         let state = self.data::<PairStorage>();
         let fee_to = Some(AccountId::from([1u8; 32])); // PSP22Ref::fee_to(&state.token_0); //TODO: fee_to
 
@@ -132,15 +121,37 @@ pub trait PairImpl:
                     let denominator = (root_k * 5) + root_k_last;
                     let liquidity = numerator / denominator;
                     if liquidity > 0 {
-                        self._mint_to(fee_to.unwrap(), liquidity);
+                        self._mint_to(fee_to.unwrap(), liquidity)?;
                     }
                 }
             }
         }
-        return_data
+        Ok(return_data)
     }
 
 
+
+    fn internal_update(&mut self, balance1: Balance, balance2: Balance) -> Result<(), UniswapV2Errors> {
+        let state = self.data::<PairStorage>();
+        let (reserve_0, reserve_1, block_timestamp_last) = (state.reserve_0, state.reserve_1, state.block_timestamp_last);
+        let time_elapsed = Self::env().block_timestamp() as u128 - block_timestamp_last;
+
+        if time_elapsed > 0 && reserve_0 != 0 && reserve_1 != 0 {
+            state.price0_cumulative_last += (uq112x112decode(uq112x112encode(state.reserve_1), state.reserve_0)) * time_elapsed;
+            state.price1_cumulative_last += (uq112x112decode(uq112x112encode(state.reserve_0), state.reserve_1)) * time_elapsed;
+        }
+
+
+        state.reserve_0 = balance1;
+        state.reserve_1 = balance2;
+        state.block_timestamp_last = Self::env().block_timestamp() as u128;
+
+
+        // emit the sync event
+
+
+        Ok(())
+    }
 
 
 }
@@ -152,4 +163,13 @@ fn internal_sqrt(_y: u128) -> u128 {
     let d2 = FixedU128::sqrt(d1); 
     let d3 = *d2.encode_as();
     d3 
+}
+
+
+fn uq112x112encode(x: u128) -> u128 {
+    x * 2u128.pow(112)
+}
+
+fn uq112x112decode(x: u128, y: u128) -> u128 {
+    x / y
 }
